24dc013bf1c4a6ba8b7a9e32f5806ef7a710c9f1
Adam Mueller <adam@freerunningtech.com>
2013-06-03 15:00:44 -0700

    Copy over new mail configuration from spree.

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,8 +1,15 @@
-class Spree::UserMailer < ActionMailer::Base
+begin
+require_dependency 'spree/base_mailer'
+rescue LoadError
+end
+
+superclass = defined?(Spree::BaseMailer) ? Spree::BaseMailer : ActionMailer::Base
+class Spree::UserMailer < superclass
   def reset_password_instructions(user)
     @edit_password_reset_url = spree.edit_user_password_url(:reset_password_token => user.reset_password_token)
 
-    mail(:to => user.email,
-         :subject => Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions))
+    subject = Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions)
+    mail_params = {:to => user.email, :from => from_address, :subject => subject}
+    mail(mail_params)
   end
 end
