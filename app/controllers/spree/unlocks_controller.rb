addb02f042ee3fb4195e759eda47ad25fbadd3d0
Brendan Deere <brendan@freerunningtechnologies.com>
2015-03-25 10:36:43 -0700

    Point after_unlock_path to new_spree_user_session
    Make sure the unlocks controller reroutes to the correct spree
    controller
    
    Change-Id: I0ee5b676413f032b8f19e486fb90b1673ed97ee6

--- app/controllers/spree/unlocks_controller.rb
+++ app/controllers/spree/unlocks_controller.rb
@@ -1,2 +1,7 @@
 class Spree::UnlocksController < Devise::UnlocksController
+
+  protected
+  def after_unlock_path_for(resource)
+    new_spree_user_session_path if is_navigational_format?
+  end
 end
