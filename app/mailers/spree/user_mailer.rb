9511000d0c5a72a1105233c01c81377b4863b892
Brendan Deere <brendan@freerunningtechnologies.com>
2015-03-26 14:59:55 -0700

    Add Unlock instructions to UserMailer
    Change-Id: I743154d298034092ea9aa3ea18052db3b455bb78

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -4,8 +4,17 @@ module Spree
       @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token)
 
       mail(:to => user.email, :from => from_address,
-          :subject => Spree::Config[:site_name] + ' ' + 
+          :subject => Spree::Config[:site_name] + ' ' +
             I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+    end
+
+    def unlock_instructions(user, token, *args)
+      @user = user
+      @token = token
+
+      mail(:to => user.email, :from => from_address,
+          :subject => Spree::Config[:site_name] + ' ' +
+            I18n.t(:subject, :scope => [:devise, :mailer, :unlock_instructions]))
     end
   end
 end
