3458c2cf7e011f05bc9105b4bc9f06ec6ff5cbe1
Karl-Aksel Puulmann <karl@heapanalytics.com>
2017-04-18 14:34:07 +0300

    Add tests for password reset functionality.

--- spec/features/password_reset_spec.rb
+++ spec/features/password_reset_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 describe "Reset Password" do
   before do
+    @user = create(:user, :email => "foobar@example.com", :password => "secret", :password_confirmation => "secret")
     ActionMailer::Base.default_url_options[:host] = "http://example.com"
   end
 
-  it "should allow a user to supply an email for the password reset" do
-    user = create(:user, :email => "foobar@example.com", :password => "secret", :password_confirmation => "secret")
+  def fill_reset_password_page!
     visit spree.login_path
     click_link "Forgot Password?"
-    fill_in "Email", :with => "foobar@example.com"
+    fill_in "Email", :with => @user.email
     click_button "Reset my password"
+    @user.reload.reset_password_token
+  end
+
+  it "should allow a user to supply an email for the password reset" do
+    fill_reset_password_page!
     page.should have_content("You will receive an email with instructions")
   end
 
@@ -19,5 +24,27 @@ describe "Reset Password" do
     click_link "Forgot Password?"
     click_button "Reset my password"
     page.should have_content("Email can't be blank")
+  end
+
+  it "should show reset page with a valid token" do
+    token = fill_reset_password_page!
+
+    visit spree.edit_password_path(reset_password_token: token)
+    # Check no redirect
+    current_path.should == spree.edit_password_path
+  end
+
+  it "should redirect to login if getting an invalid token" do
+    visit spree.edit_password_path(reset_password_token: 'this-token-does-not-exist')
+    page.should have_content('Given password reset token is either invalid or expired.')
+  end
+
+  it "should show reset page with a valid token" do
+    token = fill_reset_password_page!
+    @user.reset_password_sent_at = 2.days.ago
+    @user.save!
+
+    visit spree.edit_password_path(reset_password_token: token)
+    page.should have_content('Given password reset token is either invalid or expired.')
   end
 end
